306bf4bb0c3948d271cb8e1b3f6b964c960a649e
Fraser McKay <frasermckay97@gmail.com>
2018-04-23 10:16:01 +0100

    artist: save()

---
+++ models/specs/album_specs.rb
@@ -0,0 +1,11 @@
+require( 'minitest/autorun')
+require_relative ()
+
+
+class TestAlbum < MiniTest::Test
+
+
+  def setup
+  end
+
+end
---
+++ models/specs/artist_specs.rb
@@ -0,0 +1,11 @@
+require( 'minitest/autorun')
+require_relative ()
+
+
+class TestArtist < MiniTest::Test
+
+
+  def setup
+  end
+
+end
